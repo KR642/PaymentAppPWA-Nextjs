a0c2c903f8c956ceedea2f30f7906065e1cba306
Stark-Akshay <akshaymanoj.dms@gmail.com>
2023-09-10 20:50:00 +0100

    commit for iphone

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,7 @@ import '@/styles/globals.css'
 import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    Notification.requestPermission().then((permission) => {
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
-      } else {
-        console.log('Unable to get permission to notify.');
-      }
-    });
-  }, []);
+
   return (
   <UserAuthContextProvider>
 
--- pages/index.js
+++ pages/index.js
@@ -18,15 +18,7 @@ export default function Home() {
     }
   })
 
-  useEffect(() => {
-    Notification.requestPermission().then((permission) => {
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
-      } else {
-        console.log('Unable to get permission to notify.');
-      }
-    });
-  }, []);
+
   return (
     <>
       <Welcome />
