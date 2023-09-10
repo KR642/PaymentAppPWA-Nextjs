ba4927e7df781c7a9cbbf622af621ecff4d13aeb
Stark-Akshay <akshaymanoj.dms@gmail.com>
2023-09-10 20:39:04 +0100

    no connection why

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
-self.addEventListener('push', function(e) {
-    var options = {
-      body: e.data.text(),
-      icon: 'icon.png',
-      badge: 'badge.png'
-    };
-    e.waitUntil(
-      self.registration.showNotification('Payment App', options)
-    );
-  });
+// self.addEventListener('push', function(e) {
+//     var options = {
+//       body: e.data.text(),
+//       icon: 'icon.png',
+//       badge: 'badge.png'
+//     };
+//     e.waitUntil(
+//       self.registration.showNotification('Payment App', options)
+//     );
+//   });
   
